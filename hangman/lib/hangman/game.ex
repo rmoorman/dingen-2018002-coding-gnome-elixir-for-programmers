94b3cb44afcd140be7f68eb7fa698dac4b391b4c
Rico Moorman <rico.moorman@gmail.com>
2018-01-25 22:23:25 +0100

    Score a Good move

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -6,10 +6,12 @@ defmodule Hangman.Game do
     used: MapSet.new(),
   ]
 
+  def new_game(word) do
+    %Hangman.Game{letters: word |> String.codepoints()}
+  end
   def new_game() do
-    %Hangman.Game{
-      letters: Dictionary.random_word() |> String.codepoints()
-    }
+    Dictionary.random_word()
+    |> new_game()
   end
 
   def make_move(game = %Hangman.Game{game_state: state}, _guess) when state in [:won, :lost] do
@@ -25,9 +27,25 @@ defmodule Hangman.Game do
     Map.put(game, :game_state, :already_used)
   end
 
-  def accept_move(game, guess, _already_guessed = false) do
+  def accept_move(game, guess, _already_guessed) do
     Map.put(game, :used, MapSet.put(game.used, guess))
+    |> score_guess(Enum.member?(game.letters, guess))
   end
+
+  def score_guess(game, _good_guess = true) do
+    new_state =
+      MapSet.new(game.letters)
+      |> MapSet.subset?(game.used)
+      |> maybe_won()
+    Map.put(game, :game_state, new_state)
+  end
+
+  def score_guess(game, _not_good_guess) do
+    game
+  end
+
+  def maybe_won(true), do: :won
+  def maybe_won(_), do: :good_guess
 
   def tally(_game) do
     123
--- hangman/mix.exs
+++ hangman/mix.exs
@@ -5,7 +5,7 @@ defmodule Hangman.MixProject do
     [
       app: :hangman,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: ">= 1.5.3",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]

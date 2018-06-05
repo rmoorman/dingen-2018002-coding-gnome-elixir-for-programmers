fcbc3061bdfe5fdf70806a5e29b8363108f81b93
Rico Moorman <rico.moorman@gmail.com>
2018-06-05 23:38:20 +0200

    Add output of `mix new text_client`

---
+++ text_client/lib/text_client/interact.ex
@@ -0,0 +1,23 @@
+defmodule TextClient.Interact do
+
+  alias TextClient.State
+
+  def start() do
+    Hangman.new_game()
+    |> setup_state()
+    |> IO.inspect()
+  end
+
+  def play(state) do
+    # interact
+    state
+    |> play()
+  end
+
+  defp setup_state(game) do
+    %State{
+      game_service: game,
+      tally: Hangman.tally(game),
+    }
+  end
+end

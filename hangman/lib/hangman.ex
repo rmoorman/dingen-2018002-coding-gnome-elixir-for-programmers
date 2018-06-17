34ab6aa1aef7ff975d3c3eeab0b51a308e3c8a59
Rico Moorman <rico.moorman@gmail.com>
2018-06-17 23:27:00 +0200

    A Dynamic Cloud of Hangman Servers

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -2,7 +2,11 @@ defmodule Hangman do
 
   alias Hangman.Server
 
-  defdelegate new_game(), to: Server, as: :start_link
+  def new_game() do
+    {:ok, game_pid} = Supervisor.start_child(Hangman.Supervisor, [])
+    game_pid
+  end
+
   defdelegate tally(game_pid), to: Server
   defdelegate make_move(game_pid, guess), to: Server
 

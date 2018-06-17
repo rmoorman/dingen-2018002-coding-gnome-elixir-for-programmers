f70f0c8635b0647ccc913fe728aa8d8c3b17cac9
Rico Moorman <rico.moorman@gmail.com>
2018-06-17 13:26:32 +0200

    Pattern Matching Messages

--- scratch/procs.exs
+++ scratch/procs.exs
@@ -8,4 +8,16 @@ defmodule Procs do
     greeter(what_to_say)
   end
 
+
+  def counter(count \\ 0) when is_integer(count) do
+    receive do
+      {:add, amount} when is_integer(amount) ->
+        counter(count + amount)
+      {:reset} ->
+        counter(0)
+      msg ->
+        IO.puts "Unknown message #{inspect msg} received... Anyway, the current count is #{inspect count}"
+        counter(count)
+    end
+  end
 end

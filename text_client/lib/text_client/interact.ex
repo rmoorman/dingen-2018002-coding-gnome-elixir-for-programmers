2d5245282522edd8c593a268a98b9d8c525d0e0c
Rico Moorman <rico.moorman@gmail.com>
2018-06-19 00:10:28 +0200

    Start Remote Server when TextClient starts

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -3,7 +3,7 @@ defmodule TextClient.Interact do
   alias TextClient.{State, Player}
 
   def start() do
-    Hangman.new_game()
+    new_game()
     |> setup_state()
     |> Player.play()
   end
@@ -13,6 +13,18 @@ defmodule TextClient.Interact do
       game_service: game,
       tally: Hangman.tally(game),
     }
+  end
+
+  defp new_game() do
+    server_node = hangman_server()
+    Node.connect(server_node)
+    :rpc.call(server_node, Hangman, :new_game, [])
+  end
+
+  defp hangman_server() do
+    System.get_env()
+    |> Map.fetch!("HANGMAN_SERVER_NODE")
+    |> String.to_atom()
   end
 
 end

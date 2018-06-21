77aa0467f1220b9259c3f7484a7b6a1e9839dab1
Rico Moorman <rico.moorman@gmail.com>
2018-06-21 23:22:08 +0200

    Project Structure

--- gallows/lib/gallows_web/views/page_view.ex
+++ gallows/lib/gallows_web/views/page_view.ex
@@ -1,3 +1,13 @@
 defmodule GallowsWeb.PageView do
   use GallowsWeb, :view
+
+  def plural_of(word, 1), do: "1 #{word}"
+  def plural_of(word, count) when count < 0 do
+    {:safe, [
+      "<span style='color: red'>",
+      Plug.HTML.html_escape(plural_of(word, abs(count))),
+      "</span>"
+    ]}
+  end
+  def plural_of(word, count), do: "#{count} #{word}s"
 end

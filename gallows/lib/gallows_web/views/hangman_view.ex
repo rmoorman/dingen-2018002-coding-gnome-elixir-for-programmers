a5f0ff607189989a8b27af2efb7647af572a30c4
Rico Moorman <rico.moorman@gmail.com>
2018-06-22 19:35:32 +0200

    Wrapping Up: Adding Graphics

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -7,6 +7,10 @@ defmodule GallowsWeb.HangmanView do
     tally.letters |> Enum.join(" ")
   end
 
+  def letters_used(tally) do
+    tally.used |> Enum.join(" ")
+  end
+
   def new_game_button(conn, label) do
     button(label, to: hangman_path(conn, :create_game))
   end
@@ -14,4 +18,7 @@ defmodule GallowsWeb.HangmanView do
   def game_over?(%{game_state: game_state}) do
     game_state in [:won, :lost]
   end
+
+  def turn(left, target) when target >= left, do: nil
+  def turn(_left, _target), do: :dim
 end
